d7f76694180db67b73c229b9c92a862591ca8e08
santiago noguera <santiago.noguera@moove-it.com>
2018-02-02 15:07:28 -0300

    Initial modal structure.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import './styles/main.css';
 
 class Modal extends React.PureComponent {
   constructor(props) {
     super(props);
+
+    const { domElement } = props;
+
+    if (typeof domElement === 'string') {
+      this.domElement = document.querySelector(domElement);
+    } else {
+      this.domElement = domElement;
+    }
+
     this.state = {};
   }
-  render() {
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+    ReactDOM.createPortal(this.renderModalMarkup(), this.domElement);
+  }
+
+  componentWillReceiveProps(nextProps) {
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+  }
+
+  componentWillUnmount() {
+    ReactDOM.unmountComponentAtNode(this.domElement);
+  }
+
+  renderModalMarkup() {
+    const { children } = this.props;
+
     return (
-      <h1>React Modal</h1>
+      <div>
+        {children}
+      </div>
     );
   }
+
+  render() {
+    return null;
+  }
 }
+
+Modal.propTypes = {
+  children: React.PropTypes.any,
+};
 
 export default Modal;
