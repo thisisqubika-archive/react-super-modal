9dcbafa7af8e35e62291c249a4c548995e28ba13
Renzo Scuadroni <renzo.scuadroni@moove-it.com>
2018-02-09 15:21:44 -0300

    Add color props

--- demo/src/components/SimpleExample.js
+++ demo/src/components/SimpleExample.js
@@ -4,32 +4,44 @@ import Modal from 'react-modals';
 class SimpleExample extends Component {
   constructor() {
     super();
-    this.state = { modalIsOpen: false };
+    this.state = {
+      isModalOpen: false,
+    };
 
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
   }
 
-  showModal() {
-    this.setState({ modalIsOpen: true });
+  showModal(backgroundColor, overlayColor, closeButtonColor) {
+    this.setState({
+      isModalOpen: true,
+      backgroundColor,
+      overlayColor,
+      closeButtonColor,
+    });
   }
 
   hideModal() {
-    this.setState({ modalIsOpen: false });
+    this.setState({ isModalOpen: false });
   }
 
   render() {
+    const { isModalOpen, backgroundColor, overlayColor, closeButtonColor } = this.state;
     return (
       <div>
-        <button onClick={this.showModal}>Show Modal</button>
+        <button onClick={() => this.showModal()}>Show Light Modal</button>
+        <button onClick={() => this.showModal('#000000', 'rgba(0,0,0,0.7)', '#FFFFFF')}>Show Dark Modal</button>
         <Modal
-          isOpen={this.state.modalIsOpen}
+          isOpen={isModalOpen}
           onClose={this.hideModal}
-          closeOnOverlayClick={false}
           maxWidth="850px"
+          closeOnOverlayClick
+          backgroundColor={backgroundColor}
+          overlayColor={overlayColor}
+          closeButtonColor={closeButtonColor}
         >
-          <p style={{ fontSize: 40 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor nec feugiat nisl pretium fusce id. Massa sapien faucibus et molestie ac feugiat. Vel quam elementum pulvinar etiam non. Viverra nibh cras pulvinar mattis. Elementum eu facilisis sed odio. Pellentesque elit eget gravida cum sociis natoque penatibus et magnis. Id faucibus nisl tincidunt eget nullam. Arcu cursus vitae congue mauris rhoncus. Pharetra massa massa ultricies mi quis hendrerit dolor magna. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Volutpat sed cras ornare arcu dui vivamus arcu felis. Facilisis mauris sit amet massa vitae tortor condimentum. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper. Et ultrices neque ornare aenean euismod elementum nisi. Egestas maecenas pharetra convallis posuere morbi leo. Dui ut ornare lectus sit amet est placerat in egestas.
+          <p style={{ color: 'gray', fontSize: 40 }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor nec feugiat nisl pretium fusce id. Massa sapien faucibus et molestie ac feugiat. Vel quam elementum pulvinar etiam non. Viverra nibh cras pulvinar mattis. Elementum eu facilisis sed odio. Pellentesque elit eget gravida cum sociis natoque penatibus et magnis. Id faucibus nisl tincidunt eget nullam. Arcu cursus vitae congue mauris rhoncus. Pharetra massa massa ultricies mi quis hendrerit dolor magna. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor. Volutpat sed cras ornare arcu dui vivamus arcu felis. Facilisis mauris sit amet massa vitae tortor condimentum. Massa tincidunt nunc pulvinar sapien et ligula ullamcorper. Et ultrices neque ornare aenean euismod elementum nisi. Egestas maecenas pharetra convallis posuere morbi leo. Dui ut ornare lectus sit amet est placerat in egestas.
           </p>
         </Modal>
       </div>
--- src/components/CloseButton.js
+++ src/components/CloseButton.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CloseButton = ({ isDarkMode }) => (
-  isDarkMode ?
-    <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-      <title>ic-close-white</title>
-      <desc>Created with Sketch.</desc>
+const CloseButton = ({ onClose, closeButtonColor }) => (
+  <div onClick={onClose} className="close-button">
+    <svg width="12px" height="12px" viewBox="0 0 24 24" version="1.1">
+      <title>ic-close</title>
       <defs />
       <g
         id="Icon-Close"
@@ -17,42 +16,23 @@ const CloseButton = ({ isDarkMode }) => (
       >
         <polygon
           id="ic-close-white"
-          fill="#FFFFFF"
+          fill={closeButtonColor}
           fillRule="nonzero"
           // eslint-disable-next-line max-len
           points="1398 557.418057 1395.58286 555 1386 564.586483 1376.41714 555 1374 557.418057 1383.58286 567.00454 1374 576.591023 1376.41714 579.009081 1386 569.422598 1395.58286 579.009081 1398 576.591023 1388.41714 567.00454"
         />
       </g>
-    </svg> :
-    <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-      <title>ic-close-black</title>
-      <desc>Created with Sketch.</desc>
-      <defs />
-      <g
-        id="Icon-Close"
-        stroke="none"
-        strokeWidth="1"
-        fill="none"
-        fillRule="evenodd"
-        transform="translate(-1374.000000, -496.000000)"
-      >
-        <polygon
-          id="ic-close-black"
-          fill="#000000"
-          fillRule="nonzero"
-          // eslint-disable-next-line max-len
-          points="1398 498.418057 1395.58286 496 1386 505.586483 1376.41714 496 1374 498.418057 1383.58286 508.00454 1374 517.591023 1376.41714 520.009081 1386 510.422598 1395.58286 520.009081 1398 517.591023 1388.41714 508.00454"
-        />
-      </g>
     </svg>
+  </div>
 );
 
-CloseButton.propTypes = {
-  isDarkMode: PropTypes.bool,
+CloseButton.defaultProps = {
+  onClose: () => {},
 };
 
-CloseButton.defaultProps = {
-  isDarkMode: false,
+CloseButton.propTypes = {
+  onClose: PropTypes.func,
+  closeButtonColor: PropTypes.string.isRequired,
 };
 
 export default CloseButton;
